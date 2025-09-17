f98e49ea13ad698b7e9c623762a65b69dd1218eb
Dagmawi <dagijossy18@gmail.com>
2025-09-17 18:08:52 +0300

    select edit

--- src/common/ui/Select.tsx
+++ src/common/ui/Select.tsx
@@ -32,7 +32,7 @@ export interface SelectProps
   /**
    * Custom icon to display within the loading option.
    */
-  loadingOptionIcon?: React.ReactNode;
+  
   /**
    * The icon to display in the select.
    */
@@ -70,7 +70,7 @@ const IconWrapper = ({ icon, iconPosition, className }: { icon?: React.ReactNode
 };
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps & MotionProps>(
-  ({ className, variant, size, isLoading = false, loadingIcon, optionsLoading = false, loadingOptionText = "Loading options...", loadingOptionIcon, icon, iconPosition = "left", iconWrapperClassName, dropdownIcon = <FiChevronDown />, dropdownIconWrapperClassName, children, ...props }, ref) => {
+  ({ className, variant, size, isLoading = false, loadingIcon, optionsLoading = false, loadingOptionText = "Loading options...", icon, iconPosition = "left", iconWrapperClassName, dropdownIcon = <FiChevronDown />, dropdownIconWrapperClassName, children, ...props }, ref) => {
     const MotionSelect = motion.select;
 
     const leftIconPadding = icon && iconPosition === "left" ? "pl-10" : "";
@@ -98,18 +98,6 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps & MotionProps>(
         >
           {optionsLoading ? (
             <option disabled value="">
-              {loadingOptionIcon ? (
-                <span className="mr-2 inline-flex items-center justify-center">
-                  {loadingOptionIcon}
-                </span>
-              ) : (
-                <span className="mr-2 inline-flex items-center justify-center">
-                  <svg className="animate-spin h-4 w-4 text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                  </svg>
-                </span>
-              )}
               {loadingOptionText}
             </option>
           ) : (
